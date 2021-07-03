449f4204a40cd244b64c2124210160ecf82ade59
vamseeuxd <vamsee.uxd@gmail.com>
2021-07-03 19:53:41 +0530

    Added Empty Page and Routing

---
+++ src/app/pages/manage-projects/manage-projects.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'ngx-manage-projects',
+  styleUrls: ['./manage-projects.component.scss'],
+  templateUrl: './manage-projects.component.html',
+})
+export class ManageProjectsComponent {
+
+
+}
---
+++ src/app/pages/manage-projects/manage-projects.module.ts
@@ -0,0 +1,41 @@
+import { ChartsModule } from './../charts/charts.module';
+import { NgModule } from '@angular/core';
+import {
+  NbActionsModule,
+  NbButtonModule,
+  NbCardModule,
+  NbTabsetModule,
+  NbUserModule,
+  NbRadioModule,
+  NbSelectModule,
+  NbListModule,
+  NbIconModule,
+} from '@nebular/theme';
+import { NgxEchartsModule } from 'ngx-echarts';
+
+import { ThemeModule } from '../../@theme/theme.module';
+import { ManageProjectsComponent } from './manage-projects.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  imports: [
+    FormsModule,
+    ThemeModule,
+    NbCardModule,
+    NbUserModule,
+    NbButtonModule,
+    NbTabsetModule,
+    NbActionsModule,
+    NbRadioModule,
+    NbSelectModule,
+    NbListModule,
+    NbIconModule,
+    NbButtonModule,
+    NgxEchartsModule,
+    ChartsModule,
+  ],
+  declarations: [
+    ManageProjectsComponent,
+  ],
+})
+export class ManageProjectsModule { }
